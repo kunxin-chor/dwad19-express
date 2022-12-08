f4101031a0cbd7acf01be0117d02b4654946062d
kunxin-chor <chorkunxin@yahoo.com>
2022-12-08 13:09:25 +0000

    Added C of the CRUD

--- lab07/index.js
+++ lab07/index.js
@@ -7,6 +7,12 @@ const app = express();
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
+// enable the use of forms in express
+// if not done, then req.body is always undefined
+app.use(express.urlencoded({
+    extended: false
+}))
+
 // setup handlebars
 // 1. wax-on
 // 2. [NEW] setup the 189 handlebars helpers
@@ -62,6 +68,48 @@ app.get('/', function(req,res){
     })
 })
 
+// add a food record (display the form)
+app.get("/add-food", function(req,res){
+    res.render('add-food')
+})
+
+// process the form for add food record
+app.post('/add-food', function(req,res){
+    console.log(req.body);
+    const foodName = req.body.foodName;
+    const calories = req.body.calories;
+    const meal = req.body.meal;
+
+    // assume no checkboxes were checked
+    let selectedTags = [];
+
+    // 1. check if req.body.tags is an array (> 1 selected)
+    if (Array.isArray(req.body.tags)) {
+        selectedTags = req.body.tags;
+    } else if (req.body.tags){
+        // 2. if req.body.tags is not an array, then checked whether it is truthly
+        // (i.e make sure req.body.tags is not undefined). If so, then it must be a single
+        // string (i.e the user only select one checkbox)
+        selectedTags.push(req.body.tags);
+    }   
+    // by the end of if...else, selectedTags will be  an array regardless
+    // if the user selected zero, one or more than one checkboxes
+
+    // create the food record
+    const newFoodRecord = {
+        'id': Math.floor(Math.random() * 100000 + 1),
+        'foodName': foodName,
+        'calories': calories,
+        'meal': meal,
+        'tags': selectedTags
+    }
+
+    // add the new food record to the database
+    foodRecords.push(newFoodRecord);
+
+    // redirect the client (aka) browser) to a different URL
+    res.redirect('/')
+})
 
 
 // make sure app.listen is the last code to execute
